ac2c427586bd1cbab38df03dcae63bff7c43dc09
calebbabin <object.caleb@gmail.com>
2022-01-29 19:06:44 -0600

    add foreground clouds

--- src/spriteClouds.js
+++ src/spriteClouds.js
@@ -7,7 +7,7 @@ const canvas = document.createElement('canvas');
 canvas.height = canvas.width = 32;
 const material = new THREE.MeshBasicMaterial({
 	map: new THREE.Texture(canvas),
-	transparent: false,
+	transparent: true,
 	side: THREE.BackSide,
 });
 
@@ -53,33 +53,40 @@ material.onBeforeCompile = function (shader) {
 		vec3 highlight = vec3(255.0, 255.0, 255.0);
 
 		vec3 st = vWorldPosition.xyz * 0.02;
+		
+		if (vWorldPosition.z > 0.0) {
+			st.xyz *= 110.0;
+		}
 
 		float r_noise = snoise(vec3(st.x, st.y - u_time * .1, st.z + u_time * .01));
 		r_noise += snoise(vec3(st.x * 0.5, st.y * 0.5 - u_time * .1, st.z * 0.5 + u_time * .01)) * 0.5;
 		float radians = pow(3.0, r_noise) * PI * 0.02;
 		vec3 uv = vec3(cos(radians), sin(radians), cos(radians)) * 0.2 * (r_noise * 3.0);
-		
+
 		float color = snoise(vec3(st.x + uv.x, st.y, st.z - u_time * 0.1)) * 0.5 + 0.5;
 
 		color += snoise(vec3(st.x - uv.x, st.y, st.z - u_time * 0.05) * 0.5) * 0.5;
 
-		//float color = abs(mod(floor((st.x + uv.x) * 10.0), 2.0));
-
 		float alpha = 1.0;
-		
+
 		// fade higher pixels out
 		alpha *= max(0.0, min(1.0, 1.0 - vWorldPosition.y * 0.0025));
-		
+
 		// fade out a "tunnel" close to the x/y center
-		//float fadeDistance = 1.0; // ThreeJS units
+		float fadeDistance = 1.2; // ThreeJS units
 		//alpha *= pow(max(0.0, min(1.0, distance(st.xy, vec2(0.0, 0.0)) / fadeDistance)), 1.0);
+
+		if (vWorldPosition.z > 0.0) {
+			//alpha *= max(0.0, min(1.0, 1.0 - vWorldPosition.y * 0.25));
+			alpha *= pow(max(0.0, min(1.0, distance(vWorldPosition.xy, vec2(0.0, 1.5)) / fadeDistance)), 1.0);
+		}
 
 		// fade in further pixels
 		//alpha = alpha + min(1.0, max(0.0, (-(vWorldPosition.z + 10.0) * .0005)));
 
 		diffuseColor = vec4(
-			vec3(0.5, 0.75, 1.0) * (pow(alpha, 6.0) * color),
-			1.0
+			vec3(0.5, 0.75, 1.0),
+			(pow(alpha, 6.0) * color)
 		);
 	`)}`;
 };
@@ -89,12 +96,6 @@ material.customProgramCacheKey = function () {
 	return parseInt(window.shaderPID++); // some random ish number
 };
 
-/*for (let index = 0; index < 8; index++) {
-	const plane = new THREE.Mesh(geometry, material);
-	plane.position.z = -index * 10 - 6;
-	mesh.add(plane);
-}*/
-
 const sphere = new THREE.Mesh(
 	new THREE.SphereGeometry(150, 32, 32),
 	material
@@ -102,5 +103,13 @@ const sphere = new THREE.Mesh(
 sphere.position.z = -7;
 sphere.position.y = 74;
 mesh.add(sphere);
+
+const foreground = new THREE.Mesh(
+	new THREE.PlaneBufferGeometry(20, 20),
+	material
+);
+foreground.rotation.y = Math.PI;
+foreground.position.z = 4;
+mesh.add(foreground);
 
 export default mesh;
