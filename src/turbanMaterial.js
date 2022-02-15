ebba9cd9c71d83ef6de3e1c389ebaf125d58bed7
calebbabin <object.caleb@gmail.com>
2022-02-14 19:58:55 -0600

    slow down turban material offset

--- src/turbanMaterial.js
+++ src/turbanMaterial.js
@@ -31,7 +31,7 @@ function generateTurbanMat(options) {
 			// scales based on how high the vertex is from the base
 			float transformScale = max(0.0, (position.y + 2.0) / 5.0);
 
-			float time = u_time * 0.1;
+			float time = u_time * 0.065;
 			float hatNoise = snoise(vec3(position.y * 0.5 - time, position.x * 0.1, position.z * 0.1 + time * 0.25));
 			transformed += vec3(
 				-sin(hatNoise * 3.14),
@@ -42,30 +42,6 @@ function generateTurbanMat(options) {
 		);
 
 		shimmeryMat.userData.shader = shader;
-
-		shader.fragmentShader = `
-		uniform float u_time;
-		${webGLSimplex3DNoise.split('float snoiseOffset')[0]}
-		
-		${shader.fragmentShader.replace('#include <alphamap_fragment>',`
-			#include <alphamap_fragment>
-
-			float alphaMult = 0.0;
-
-			float bnScale = 3.0;
-			float timeScale = 0.3;
-			alphaMult += (1.0 + 0.5 * snoise(vec3(vUv.x * bnScale, vUv.y * bnScale - u_time * timeScale,  u_time * timeScale * 0.1)));
-			
-			float bnScale2 = bnScale * 3.0;
-			float timeScale2 = timeScale * 1.5;
-			alphaMult += (1.0 + 0.5 * snoise(vec3(vUv.x * bnScale2, vUv.y * bnScale2 - u_time * timeScale2, u_time * -timeScale2)));
-
-			float bnScale3 = bnScale2 * 1.0;
-			float timeScale3 = timeScale2 * 0.25;
-			alphaMult -= 0.5 * (1.0 + 0.5 * snoise(vec3(vUv.x * bnScale3, vUv.y * bnScale3, u_time * timeScale3)));
-
-			diffuseColor.a *= alphaMult / 2.0;
-		`)}`;
 	};
 
 	// Make sure WebGLRenderer doesn't reuse a single program
